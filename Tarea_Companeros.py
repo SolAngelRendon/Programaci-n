7af0d77f5e0a0bea0caeecbae2a03cecd0ad7584
SolAngelRendon <167156352+SolAngelRendon@users.noreply.github.com>
2024-04-15 20:13:38 -0700

    Add files via upload

---
+++ Tarea_Companeros.py
@@ -0,0 +1,15 @@
+companeros = { "Cesar" : 10,
+    "Nathalia" : 10,
+    "Aramis" : 9, 
+    "Maribel" : 8,
+    "Emmanuel" : 7, 
+    "Sahid" : 6,
+    "Jaime" : 10,
+    "Froylan" : 7,
+    "Joseph" : 8,
+    "Angel" : 9
+    
+}
+
+for clave, valor in companeros.items() :
+    print(clave,valor)
